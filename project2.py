7353c7c197fb24588c6ee6a2757f468c2f8a8f8a
Austin Rose <atrose@austin-mac-mini.res.wpi.net>
2015-03-04 22:16:41 -0500

    auto

--- project2.py
+++ project2.py
@@ -38,14 +38,7 @@ options, _ = parser.parse_args()
 options.port = int(options.port)
 options.sample_interval = int(options.port)
 
-def receive_and_play(host, port):
-    receive_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    receive_socket.bind((host, port))
-    receive_socket.listen(1)
-    connection, address = receive_socket.accept()
-
-    send_thread(*address)
-
+def receive_and_play(connection):
     device = alsaaudio.PCM(type=alsaaudio.PCM_PLAYBACK,
                            mode=alsaaudio.PCM_NONBLOCK,
                            card="default")
@@ -63,12 +56,7 @@ def receive_and_play(host, port):
     connection.close()
 
 
-def record_and_send(host, port):
-    send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    send_socket.connect((host, port))
-
-    receive_thread('', port)
-
+def record_and_send(sock):
     device = alsaaudio.PCM(type=alsaaudio.PCM_CAPTURE,
                            mode=alsaaudio.PCM_NONBLOCK,
                            card='default')
@@ -81,24 +69,34 @@ def record_and_send(host, port):
     while True:
         l, data = device.read()
         if l:
-            send_socket.send(data)
+            sock.send(data)
 
-    send_socket.close()
+    sock.close()
 
 
 def receive_thread(host, port):
-    receive_thread = Thread(target=receive_and_play, args=('', options.port))
+    receive_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    receive_socket.bind((host, port))
+    receive_socket.listen(1)
+    connection, address = receive_socket.accept()
+
+    receive_thread = Thread(target=receive_and_play, args=(connection,))
     receive_thread.setDaemon(True)
     receive_thread.start()
 
 def send_thread(host, port):
-    send_thread = Thread(target=record_and_send, args=(host, port))
+    send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    send_socket.connect((host, port))
+
+    send_thread = Thread(target=record_and_send, args=(send_socket,))
     send_thread.setDaemon(True)
     send_thread.start()
 
-if options.host is None:
+try:
+    send_thread(options.host, options.port+1)
     receive_thread('', options.port)
-else:
+except:
+    receive_thread('', options.port+1)
     send_thread(options.host, options.port)
 
 while True:
